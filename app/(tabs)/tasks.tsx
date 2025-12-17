7c3713575bd411292b2dcef5e711b38a6ca1ed0b
YifeiTi <160953584+YifeiTi@users.noreply.github.com>
2025-12-16 20:47:54 -0500

    added tasks points

--- app/(tabs)/tasks.tsx
+++ app/(tabs)/tasks.tsx
@@ -15,26 +15,37 @@ type Task = {
 export default function Tasks(){
     const router = useRouter();
 
-    const[tasks] = useState<Task[]>([
+    const[tasks, setTasks] = useState<Task[]>([
         {id: "1", title: "Go to the Gym", points: 10, completed: false},
         {id: "2", title: "Cook a Meal", points: 5, completed: false},
         {id: "3", title: "Finish Project", points: 15, completed: false},
         {id: "4", title: "Walk the Dog", points:3, completed: false},
     ]);
     const handleTaskPress = ( task: Task) =>{
-        console.log("Pressed:", task.title);
-        
+        const updatedTasks = tasks.map((t) =>{
+            if (t.id === task.id){
+                return{
+                    ...t,//expands the component t into its individual components
+                    completed: !t.completed, //change completed to the opposite of what it was
+                };
+            }
+            return t;
+        });
+        setTasks(updatedTasks);
     };
 
+    const totalPoints = tasks
+        .filter((task) => task.completed).reduce((sum, task) => sum + task.points, 0);
     return(
         <View style = {styles.container}>
             <Text style = {styles.title}>Tasks</Text>
+            <Text style = {styles.points}> Total Points: {totalPoints}</Text>
             <FlatList 
                 data= {tasks}
                 keyExtractor={(item) => item.id}
                 renderItem = {({item}) =>( 
-                <Pressable style = {styles.taskItem} onPress ={()=> handleTaskPress(item)}>
-                    <Text style = {styles.taskTitle}> {item.title}</Text>
+                <Pressable style = {[styles.taskItem, item.completed && styles.taskCompleted]} onPress ={()=> handleTaskPress(item)}>
+                    <Text style = {[styles.taskTitle, item.completed && styles.taskTitleCompleted]}> {item.title}</Text>
                     <Text style = {styles.taskPoints}>{item.points} pts</Text>
                 </Pressable>
             )}/>
@@ -72,5 +83,16 @@ const styles = StyleSheet.create({
         color: "#4A90E2",
         fontWeight:"600",
     },
-   
+    taskCompleted:{
+        backgroundColor: "#d3f9d8",
+    },
+    taskTitleCompleted:{
+        textDecorationLine: "line-through",
+        color: "#777",
+    },
+   points:{
+        fontWeight: "600",
+        fontSize: 20,
+        marginBottom: 16,
+   }
 })
