1a23d10bac0ecad37e6c45121dc8c354b1aa57f9
YifeiTi <160953584+YifeiTi@users.noreply.github.com>
2025-12-21 20:20:56 -0500

    integrated redeem logic into stats screen

--- app/(tabs)/stats.tsx
+++ app/(tabs)/stats.tsx
@@ -1,5 +1,6 @@
-import {Text, View, StyleSheet} from "react-native";
+import {Text, View, StyleSheet, Pressable, FlatList} from "react-native";
 import {useTask} from "@/context/TasksContext"
+import { useRedeem } from "@/context/RedeemContext";
 
 export default function Stats(){
     const {tasks} = useTask();
@@ -8,11 +9,25 @@ export default function Stats(){
 
     const totalPoints = tasks.filter(t=>t.completed).reduce((sum,t)=> sum+t.points, 0)
 
+    const {currentPoints, redemptions, redeem} = useRedeem();
+
     return(
         <View style = {styles.background}>
             <Text style = {styles.title}> Stats:</Text>
             <Text> Completed Tasks: {completedTasks}</Text>
             <Text> Total Points: {totalPoints}</Text>
+            <Pressable style = {StyleSheet.redeemButton} onPress={()=> {
+                const success = redeem(50);
+                if(!success){
+                    alert("Not enough points");
+                }
+            }}><Text style = {styles.redeemText}> Redeem 50 Points! </Text> </Pressable>
+
+            <FlatList
+                data = {redemptions}
+                keyExtractor={(item)=> item.id}
+                renderItem = {({item}) => (<View style = {styles.redemptionItem}> <Text> Redeemed {item.cost} pts</Text>
+                <Text>{item.date}</Text></View>)}/>
         </View>
     )
 }
